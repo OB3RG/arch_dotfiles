1f2af7764fd969a3fe353260ec306353ff634437
Oskar Oberg <oskar@shimayama.io>
2024-04-03 21:26:29 +0200

    fix: Update instal_desktop script

--- install_desktop.sh
+++ install_desktop.sh
@@ -15,18 +15,27 @@ cd "$DOTFILES" || exit
 git clone git@github.com:OB3RG/arch_dotfiles.git $DOTFILES
 
 rm -rf \
-  "$HOME/.bashrc" \
   "$HOME/.gitconfig" \
   "$HOME/.zshrc" \
-  "$HOME/.config/starship.toml" \
-  "$HOME/.config/nvim"
+  "$DOTCONFIG/dolphinrc" \
+  "$DOTCONFIG/alacritty" \
+  "$DOTCONFIG/hypr" \
+  "$DOTCONFIG/swaylock" \
+  "$DOTCONFIG/waybar" \
+  "$DOTCONFIG/wlogout" \
+  "$DOTCONFIG/wofi" \
+  "$DOTCONFIG/lvim" 
 
-ln -s "$DOTFILES/bashrc" "$HOME/.bashrc"
 ln -s "$DOTFILES/gitconfig" "$HOME/.gitconfig"
-ln -s "$DOTFILES/zshrc" "$HOME/.zshrc"
-ln -s "$DOTFILES/gitconfig" "$HOME/.gitconfig"
-ln -s "$DOTFILES/starship.toml" "$HOME/.config/starship.toml"
-ln -s "$DOTFILES/nvim" "$HOME/.config/nvim"
+ln -s "$DOTFILES/zsh/zshrc" "$HOME/.zshrc"
+ln -s "$DOTFILES/common/config/lvim" "$HOME/.config/lvim"
+ln -s "$DOTFILES/desktop/config/alacritty" "$DOTCONFIG/alacritty"
+ln -s "$DOTFILES/desktop/config/hypr" "$DOTCONFIG/hypr"
+ln -s "$DOTFILES/desktop/config/swaylock" "$DOTCONFIG/swaylock"
+ln -s "$DOTFILES/desktop/config/waybar" "$DOTCONFIG/waybar"
+ln -s "$DOTFILES/desktop/config/wlogout" "$DOTCONFIG/wlogout"
+ln -s "$DOTFILES/desktop/config/wofi" "$DOTCONFIG/wofi"
+ln -s "$DOTFILES/desktop/config/dolphinrc" "$DOTCONFIG/dolphinrc"
 
 cd "$HOME" || exit
 rm -f "${HOME}/.zcompdump*"
