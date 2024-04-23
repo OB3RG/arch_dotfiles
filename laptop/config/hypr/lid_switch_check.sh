da88259eadab09213669ca14b0f41d10d034ca92
Oskar Oberg <oskar@shimayama.io>
2024-04-23 22:29:11 +0200

    fixed scaling for laptop display

--- laptop/config/hypr/lid_switch_check.sh
+++ laptop/config/hypr/lid_switch_check.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 
 if grep open /proc/acpi/button/lid/LID0/state; then
-    hyprctl keyword monitor "eDP-1, preferred, auto, auto"
+    hyprctl keyword monitor "eDP-1, preferred, auto, 1.666667"
 else 
   if [[ `hyprctl monitors | grep "Monitor" | wc -l` != 1 ]]; then
     hyprctl keyword monitor "eDP-1, disable"
